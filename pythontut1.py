770c66c97aab1f5794f52a9c23a2e325bfbe35b1
Mykhaylo Dyachenko <mihaeldyachenko@gmail.com>
2024-04-15 02:22:59 -0500

    Finished with data types

---
+++ pythontut1.py
@@ -0,0 +1,29 @@
+import sys
+
+my_age = 35
+my_name = "Misha"
+
+print("Hello", my_name)
+
+str_1 = "\"This is a quote\""
+
+print(sys.maxsize)
+print(sys.float_info.max)
+
+f1 = 1.1111111111111111
+f2 = 1.1111111111111111
+f3 = f1 + f2
+print(f3)
+
+can_vote = True
+
+print("Cast", type(int(5.4)))
+print("Cast 2", type(str(5.4)))
+print("Cast 3", type(chr(97)))
+print("Cast 4", type(ord('a')))
+print("Cast", type(float(2)))
+
+num_1 = "1"
+num_2 = "2"
+print("1 + 2 =", (int(num_1) + int(num_2)))
+
